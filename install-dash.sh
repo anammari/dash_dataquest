fa703aad382d30ac9dc01d64a065484bf6a80912
Ahmad Ammari <ammariect@gmail.com>
2020-09-07 00:36:22 +1000

    bash to install required Python librarues

---
+++ install-dash.sh
@@ -0,0 +1,7 @@
+conda activate dash
+pip install dash==0.26.3  # The core dash backend
+pip install dash-renderer==0.13.2  # The dash front-end
+pip install dash-html-components==0.11.0  # HTML components
+pip install dash-core-components==0.28.1  # Supercharged components
+pip install dash_table_experiments #for tables
+pip install plotly --upgrade # Plotly graphing library
